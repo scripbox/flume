b8303c5ea6b2086759761f958c9ee201b116cf18
Vasu Adari <vasu.adari@scripbox.com>
2018-07-05 14:33:02 +0530

    Fix TestWithRedis module

--- test/support/test_with_redis.ex
+++ test/support/test_with_redis.ex
@@ -6,7 +6,7 @@ defmodule TestWithRedis do
   setup _tags do
     on_exit(fn ->
       pool_size = Config.redis_pool_size()
-      conn_key = :"#{Flume.redix_worker_prefix()}_#{pool_size - 1}"
+      conn_key = :"#{Flume.Redis.Supervisor.redix_worker_prefix()}_#{pool_size - 1}"
       keys = Redix.command!(conn_key, ["KEYS", "#{Config.get(:namespace)}:*"])
       Enum.map(keys, fn key -> Redix.command(conn_key, ["DEL", key]) end)
     end)
