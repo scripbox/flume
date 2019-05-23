4c054224eed6c1bc9a915994a0c5d2745837fe64
Rahul Jayaraman <rahul.jayaraman@hotmail.com>
2019-05-23 12:48:40 +0530

    Reduce default load, pre-seed queues

--- redis_benchmark.ex
+++ redis_benchmark.ex
@@ -4,13 +4,38 @@ defmodule RedisBenchmark do
 
   @namespace Config.namespace()
 
-  @default_poll_timeout 500
   @rate_limit_opts %{rate_limit_count: 50_000, rate_limit_scale: 1000}
 
-  def start_enqueue_dequeue(dequeue_fn, count, queues, dequeue_batch, enqueue_concurrency) do
+  @defaults [
+    count: 1_000_000,
+    queues: 20,
+    dequeue_batch: 1000,
+    enqueue_concurrency: 10000,
+    # Gets multiplied by count and those many jobs get pre-seeded
+    pre_seed_multiplier: 5,
+    dequeue_poll_timeout: 500
+  ]
+
+  def defaults, do: @defaults
+
+  def start_enqueue_dequeue(
+        dequeue_fn,
+        _opts = %{
+          count: count,
+          queues: queues,
+          dequeue_batch: dequeue_batch,
+          enqueue_concurrency: enqueue_concurrency,
+          pre_seed_multiplier: pre_seed_multiplier,
+          dequeue_poll_timeout: dequeue_poll_timeout
+        }
+      ) do
     clear_redis()
     jobs_queue_mapping = build_jobs_queue_mapping(count, queues)
-    dequeue_tasks = start_dequeue(jobs_queue_mapping, dequeue_fn, dequeue_batch)
+    pre_seed_queues(jobs_queue_mapping, pre_seed_multiplier)
+
+    dequeue_tasks =
+      start_dequeue(jobs_queue_mapping, dequeue_fn, dequeue_batch, dequeue_poll_timeout)
+
     enqueue(jobs_queue_mapping, enqueue_concurrency)
     Enum.each(dequeue_tasks, &Process.exit(&1, :normal))
     clear_redis()
@@ -22,14 +47,28 @@ defmodule RedisBenchmark do
     poll(function, poll_timeout)
   end
 
-  defp start_dequeue(jobs_queue_mapping, dequeue_fn, dequeue_batch) do
+  defp start_dequeue(jobs_queue_mapping, dequeue_fn, dequeue_batch, dequeue_poll_timeout) do
     Enum.map(jobs_queue_mapping, fn {_jobs, queue} ->
       {:ok, pid} =
-        start_poll_server(fn ->
-          dequeue_fn.(queue, dequeue_batch)
-        end)
+        start_poll_server(
+          fn ->
+            dequeue_fn.(queue, dequeue_batch)
+          end,
+          dequeue_poll_timeout
+        )
 
       pid
+    end)
+  end
+
+  defp pre_seed_queues(jobs_queue_mapping, multiplier) do
+    enqueue = fn {job_ids, queue} ->
+      jobs = Enum.map(job_ids, fn id -> [:worker, :perform, [id]] end)
+      {:ok, _} = Manager.bulk_enqueue(@namespace, queue, jobs)
+    end
+
+    Enum.each(1..multiplier, fn _ ->
+      Enum.each(jobs_queue_mapping, enqueue)
     end)
   end
 
@@ -52,7 +91,7 @@ defmodule RedisBenchmark do
     |> Enum.each(&Task.await(&1, :infinity))
   end
 
-  defp start_poll_server(function, poll_timeout \\ @default_poll_timeout) do
+  defp start_poll_server(function, poll_timeout) do
     Task.start_link(__MODULE__, :poll, [function, poll_timeout])
   end
 
@@ -99,28 +138,33 @@ defmodule RedisBenchmark do
   end
 end
 
-count = 10_000_000
-queues = 100
-batch = 1000
-enqueue_concurrency = 10000
+user_options =
+  OptionParser.parse(
+    System.argv(),
+    strict: [
+      count: :integer,
+      queues: :integer,
+      dequeue_batch: :integer,
+      enqueue_concurrency: :integer,
+      pre_seed_multiplier: :integer,
+      dequeue_poll_timeout: :integer
+    ]
+  )
+  |> elem(0)
+
+opts =
+  RedisBenchmark.defaults()
+  |> Keyword.merge(user_options)
+  |> Enum.into(%{})
+
+IO.inspect("Running benchmark with config")
+IO.inspect(opts)
 
 Benchee.run(%{
   "old_enqueue_dequeue" => fn ->
-    RedisBenchmark.start_enqueue_dequeue(
-      &RedisBenchmark.old_bulk_dequeue/2,
-      count,
-      queues,
-      batch,
-      enqueue_concurrency
-    )
+    RedisBenchmark.start_enqueue_dequeue(&RedisBenchmark.old_bulk_dequeue/2, opts)
   end,
   "new_enqueue_dequeue" => fn ->
-    RedisBenchmark.start_enqueue_dequeue(
-      &RedisBenchmark.new_bulk_dequeue/2,
-      count,
-      queues,
-      batch,
-      enqueue_concurrency
-    )
+    RedisBenchmark.start_enqueue_dequeue(&RedisBenchmark.new_bulk_dequeue/2, opts)
   end
 })
