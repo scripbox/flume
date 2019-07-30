32db350bf640ad2f7161a18d12051ba33b8c765d
Nitin Misra <nitin.misra23@gmail.com>
2019-07-30 23:27:47 +0530

    Fix elixir 1.7 warnings

--- lib/flume/support/time.ex
+++ lib/flume/support/time.ex
@@ -2,23 +2,23 @@ defmodule Flume.Support.Time do
   import DateTime, only: [utc_now: 0, to_unix: 2, from_unix!: 2]
 
   def offset_from_now(offset_in_ms, current_time \\ utc_now()) do
-    now_in_µs = current_time |> to_unix(:microseconds)
+    now_in_µs = current_time |> to_unix(:microsecond)
     offset_in_µs = offset_in_ms * 1_000
 
     now_in_µs
     |> Kernel.+(offset_in_µs)
     |> round()
-    |> from_unix!(:microseconds)
+    |> from_unix!(:microsecond)
   end
 
   def offset_before(offset_in_ms, current_time \\ utc_now()) do
-    now_in_µs = current_time |> to_unix(:microseconds)
+    now_in_µs = current_time |> to_unix(:microsecond)
     offset_in_µs = offset_in_ms * 1_000
 
     now_in_µs
     |> Kernel.-(offset_in_µs)
     |> round()
-    |> from_unix!(:microseconds)
+    |> from_unix!(:microsecond)
   end
 
   def time_to_score(time \\ utc_now()) do
@@ -28,7 +28,7 @@ defmodule Flume.Support.Time do
   end
 
   def unix_seconds(time \\ utc_now()) do
-    to_unix(time, :microseconds) / 1_000_000.0
+    to_unix(time, :microsecond) / 1_000_000.0
   end
 
   def format_current_date(current_date) do
