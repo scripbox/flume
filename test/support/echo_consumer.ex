07bc617a06aee04b8da7bddd0617394be4220a65
Anantha Kumaran <ananthakumaran@gmail.com>
2022-06-22 11:50:47 +0530

    make sure not more than one fetch_event message is scheduled
    If a new fetch_events message is scheduled just based on the demand
    > 0 predicate, more than one message could be scheduled and this could
    lead to a backlog of fetch_events messages in the message queue
    
    Make sure there are no outstanding schedule before adding a new schedule

--- test/support/echo_consumer.ex
+++ test/support/echo_consumer.ex
@@ -1,12 +1,21 @@
 defmodule EchoConsumer do
   use GenStage
 
-  def start_link(producer, owner, name: process_name) do
-    GenStage.start_link(__MODULE__, {producer, owner}, name: process_name)
+  def start_link(producer, owner, options) do
+    GenStage.start_link(
+      __MODULE__,
+      %{
+        producer: producer,
+        owner: owner,
+        max_demand: Keyword.get(options, :max_demand, 1),
+        min_demand: Keyword.get(options, :min_demand, 0)
+      },
+      name: Keyword.fetch!(options, :name)
+    )
   end
 
-  def init({producer, owner}) do
-    {:consumer, owner, subscribe_to: [{producer, min_demand: 0, max_demand: 1}]}
+  def init(%{producer: producer, owner: owner, min_demand: min_demand, max_demand: max_demand}) do
+    {:consumer, owner, subscribe_to: [{producer, min_demand: min_demand, max_demand: max_demand}]}
   end
 
   def handle_events(events, _, owner) do
